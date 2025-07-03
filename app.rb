e69bee8dc252dc3fe592d87a43fe33d8ea64e604
mory-yuma <129245023+mory-yuma@users.noreply.github.com>
2025-07-03 13:29:35 +0900

    データベースと接続しCRUD操作が可能に

--- app.rb
+++ app.rb
@@ -1,7 +1,15 @@
 require "sinatra"
 require "sinatra/reloader"
+require "mysql2"
 
-$todos = []
+
+DB = Mysql2::Client.new(
+  host: "localhost",
+  username: "root",
+  password: "",
+  database: "todo_app", 
+  symbolize_keys: true      # 結果のキーを文字列でなくシンボルで返す（:name など）
+)
 
 # getリクエスト = ブラウザ(ChromeやSafari)がサーバーに「このページの内容をください」とお願いするリクエスト
 # "/"に対してgetリクエストを送ってきたらブロックの処理をしますよ
@@ -25,18 +33,19 @@ get "/" do
       <input type="submit">
     </form>
     <ul>
-    #{$todos.each_with_index.map do |t, i|
-    checked = t[:done] ? "checked" : ""
-    done_class = t[:done] == true ? "done" : ""
+    #{DB.query('SELECT * FROM todos').map do |t|
+    checked = t[:done].to_i == 1 ? "checked" : ""
+    done_class = t[:done].to_i == 1 ? "done" : ""
+    task_id = t[:id].to_i
     # onchange =JSのイベント属性 チェックボックスの値が変わったときに動くイベント
     # this.form.submit() = ここではinput要素のあるform要素のフォームを送信 となる
     <<~ITEM
       <li class = "#{done_class}">
-        <form action="/toggle/#{i}" method="post" style="display:inline">
+        <form action="/toggle/#{task_id}" method="post" style="display:inline">
           <input type="checkbox" onchange="this.form.submit()" #{checked}>
         </form>
         #{t[:name]}
-        <a href="/delete/#{i}">削除</a>
+        <a href="/delete/#{task_id}">削除</a>
       </li>
     ITEM
     end.join}
@@ -48,19 +57,27 @@ end
 
 # "/add"にpostリクエストが送られた時に実行
 post "/add" do
-  $todos << { name: params[:task], done: false}
+  task = params[:task]
+  DB.query("INSERT INTO todos (name) VALUES ('#{DB.escape(task)}')")
   redirect "/"
 end
 
+# .escapeは文字列をエスケープするメソッド to_sは文字列化するメソッド
 get "/delete/:id" do
-  # sinatraではシンボルで値を受け取ると文字列になってしまう仕様のため.to_iで整数化
   id = params[:id].to_i
-  $todos.delete_at(id) if id >= 0 && id < $todos.size
+  DB.query("DELETE FROM todos WHERE id = #{DB.escape(id.to_s)}")
   redirect "/"
 end
+
 
 post "/toggle/:id" do 
   id = params[:id].to_i
-  $todos[id][:done] = !$todos[id][:done]
+  result = DB.query("SELECT done FROM todos WHERE id = #{id}").first
+  done = result[:done].to_i
+  if done == 0 then
+    DB.query("UPDATE todos SET done = 1 WHERE id = #{id}")
+  else
+    DB.query("UPDATE todos SET done = 0 WHERE id = #{id}")
+  end
   redirect "/"
 end
