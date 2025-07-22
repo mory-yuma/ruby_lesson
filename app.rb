91c788de78dd01ede3df3174f9cc78c0db92674b
mory-yuma <129245023+mory-yuma@users.noreply.github.com>
2025-07-22 22:51:20 +0900

    app.rbとviews/index.erbを追加し忘れたため追加

--- app.rb
+++ app.rb
@@ -1,7 +1,16 @@
 require "sinatra"
-require "sinatra/reloader"
+# require "sinatra/reloader"
 require "mysql2"
+require "bcrypt"
+# -理解していない部分-
+require "securerandom"
 
+secret_key = SecureRandom.hex(64)  # これで起動時にだけ生成する
+
+use Rack::Session::Cookie, key: 'rack.session',
+                           path: '/',
+                           secret: secret_key
+# ---
 
 DB = Mysql2::Client.new(
   host: "localhost",
@@ -11,16 +20,62 @@ DB = Mysql2::Client.new(
   symbolize_keys: true      # 結果のキーを文字列でなくシンボルで返す（:name など）
 )
 
+# signup
+get "/signup" do
+  erb :signup
+end
+# signup処理
+post "/signup" do
+  name = params[:name]
+  email = params[:email]
+  password = params[:password]
+  password_digest = BCrypt::Password.create(password)
+  stmt = DB.prepare("INSERT INTO users (name,email,password_digest) VALUES (?,?,?)")
+  stmt.execute(name,email,password_digest)
+  redirect "/login"
+end
+
+#login
+get "/login" do
+  erb :login
+end
+# login処理
+post "/login" do
+  email = params[:email]
+  password = params[:password]
+  stmt = DB.prepare("SELECT * FROM users WHERE email = ?")
+  result = stmt.execute(email).first
+  if result && BCrypt::Password.new(result[:password_digest]) == password
+    session[:user_id] = result[:id]
+    redirect "/"
+  else
+    "メールアドレスまたはパスワードが間違っています"
+  end
+end
+
+# logout処理
+post "/logout" do
+  session.clear
+  redirect "/"
+end
+
 # getリクエスト = ブラウザ(ChromeやSafari)がサーバーに「このページの内容をください」とお願いするリクエスト
 # "/"に対してgetリクエストを送ってきたらブロックの処理をしますよ
 # aタグはgetリクエストを送る
 get "/" do
+  @current_user = nil
+  if session[:user_id]
+    stmt = DB.prepare("SELECT * FROM users WHERE id = ?")
+    @current_user = stmt.execute(session[:user_id]).first
+  end
   @categories = DB.query("SELECT id, name FROM categories")
-  @todos = DB.query("
+  stmt = DB.prepare("
       SELECT todos.*, categories.name AS category_name
       FROM todos
       INNER JOIN categories ON todos.category_id = categories.id
-    ").map do |t| {
+      WHERE todos.user_id = ?
+    ")
+    @todos = stmt.execute(session[:user_id]).map do |t| {
       id: t[:id],
       task_name: t[:name],
       category_name: t[:category_name],
@@ -34,27 +89,32 @@ end
 post "/tasks" do
   task = params[:task]
   category_id = params[:category_id].to_i
-  DB.query("INSERT INTO todos (name, category_id) VALUES ('#{DB.escape(task)}', #{category_id})")
+  user_id = session[:user_id]
+  stmt = DB.prepare("INSERT INTO todos (name, category_id, user_id) VALUES (?,?,?)")
+  stmt.execute(task,category_id,user_id)
   redirect "/"
 end
 
 # .escapeは文字列をエスケープするメソッド to_sは文字列化するメソッド
 delete "/tasks/:id" do
   id = params[:id].to_i
-  query = DB.prepare("DELETE FROM todos WHERE id = ?")
-  query.execute(id)
+  user_id = session[:user_id]
+  stmt = DB.prepare("DELETE FROM todos WHERE id = ?")
+  stmt.execute(id)
   redirect "/"
 end
 
+
 patch "/tasks/:id" do 
   id = params[:id].to_i
-  result = DB.query("SELECT is_completed FROM todos WHERE id = #{id}").first
+  stmt = DB.prepare("SELECT is_completed FROM todos WHERE id = ?")
+  result = stmt.execute(id).first
   is_completed = result[:is_completed].to_i
   if is_completed == 0 then
     is_completed = 1
   else
     is_completed = 0
   end
-  DB.query("UPDATE todos SET is_completed = #{is_completed} WHERE id = #{id}")
+  stmt = DB.prepare("UPDATE todos SET is_completed = ? WHERE id = ?").execute(is_completed,id)
   redirect "/"
 end
